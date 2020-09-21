4144a9890242a790c07575d56bc7fb714f354eda
J K <kj688895@gmail.com>
2020-09-21 22:33:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,13 +2,13 @@ class EmailAddressParser
   attr_accessor :name, :csv_emails
 
   def initialize(csv_emails)
-    @name = name 
+    @name = name
     @csv_emails = csv_emails
   end
 
-  def parse 
-    csv_emails.split.collect do |address| 
+  def parse
+    csv_emails.split.collect do |address|
       address.split(',')
-    end.flatten.uniq 
+    end.flatten.uniq
   end 
-end 
+end
